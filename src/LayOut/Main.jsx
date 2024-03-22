b546d143156787c05b4fdb113693a755d5798fd8
Hasan Arif <mdhasanarifh@gmail.com>
2024-03-22 23:52:24 +0600

    banner added

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Pages/Home/Shared/Navbar/Navbar";
-import Footer from "../Pages/Home/Shared/Footer/Footer";
+import Footer from "../Pages/Home/Shared/footer/Footer";
 
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,11 @@ import {
 import router from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className="max-w-6xl mx-auto">
-    <React.StrictMode>
+ <div className='max-w-3xl mx-auto'>
+     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>,
-  </div>
+ 
+ </div>
+ 
 )
